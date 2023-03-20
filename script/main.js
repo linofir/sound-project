bd20177afdfec9f441b1e4fb15ec079e9afbaebe
linofir <firmanoster@gmail.com>
2023-03-20 20:18:39 -0300

    eventkeys add

--- script/main.js
+++ script/main.js
@@ -2,8 +2,16 @@ const elementosTeclado = document.querySelectorAll('.teclado')
 
 //Ouvindo os elementos para chamar a função com parâmetro correspondente
 elementosTeclado.forEach((e) => {
+    e.addEventListener('keydown', (event) => {
+        if (event.code === 'Enter' || event.code === 'Space') {
+            event.target.classList.add('ativa')
+            e.addEventListener('keyup', (event) => {
+                event.target.classList.remove('ativa')
+                console.log(event)
+            } )
+        }
+    })
     e.addEventListener('click', (event) => {
-        console.log(event.target.dataset.som)
         tocarSom(event.target.dataset.som)
     })
 
@@ -11,7 +19,7 @@ elementosTeclado.forEach((e) => {
 //Identifica o som pelo parâmetro extraido no onclick
 function tocarSom(som) {
     const audio = document.getElementById(`som_tecla_${som}`)
-    console.log(audio.__proto__)
+    //console.log(audio.__proto__)
     audio.play()
 }
 
